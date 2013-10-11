2317b6f546a07ba7b7d59cac08b54cd139129ada
Taylor Southwick <tasou@microsoft.com>
2013-10-11 12:11:28 -0700

    Moving Logs setup to its own script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,5 @@
 ﻿Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-$variable:preference:log:path = "C:\PSLogs"
-
-function Get-Log { Get-ChildItem $variable:preference:log:path\*.log }
-function Search-Log([string] $text)
-{
-	function convert-date([string] $date)
-	{
-		[System.DateTime]::ParseExact($date.Replace(".log",""), "yyyyMMdd-HHmmss", [System.Globalization.CultureInfo]::InvariantCulture)
-	}
-	Get-Log | Select-String $text | Select FileName, LineNumber | Group-Object FileName | Select Name, Count, Group | % { New-Object PSObject -Property @{Date=convert-date($_.Name); FileName="$variable:preference:log:path\"+$_.Name; Line= $_.Group | % {$_.LineNumber}; Count = $_.Count} } | Sort -Descending -Property Date | Format-List
-}
-
 function Get-Checksum
 {
     Param (
@@ -81,25 +69,8 @@ Pop-Location
 $GitPromptSettings.WorkingForegroundColor="Red"
 $GitPromptSettings.UntrackedForegroundColor="Red"
 
-if(!(Test-Path $variable:preference:log:path))
-{
-	New-Item -ItemType Directory -Path $variable:preference:log:path
-}
-
-$pathName = "$variable:preference:log:path\" + ([System.DateTime]::Now.ToString("yyyyMMdd-HHmmss")) + ".log"
-
-try
-{
-	Start-Transcript -Path $pathName
-	Write-Host ""
-}
-catch [Exception]
-{ }
-
-Remove-Variable pathName
-
 # Include Other script files
 $path = $PSCommandPath | Split-Path -Parent
 
 . $path\SetupVSEnvironment.ps1
-
+. $path\Logs.ps1

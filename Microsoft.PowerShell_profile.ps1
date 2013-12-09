dd5ffded042f2951518faf5681689b6e7ece8155
Taylor Southwick <twsouthwick@outlook.com>
2013-12-09 09:15:00 -0800

    Updating powershell scripts to load modules from paths

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,11 @@
-﻿Import-Module posh-git
+﻿$path="$env:userprofile\Documents\WindowsPowerShell\Modules"
+$pscxpath="C:\Program Files (x86)\PowerShell Community Extensions\Pscx3"
+
+Import-Module $path\posh-git
 #Import-Module PsGet
-Import-Module pscx -force -arg @{CD_EchoNewLocation = $false}
-Import-Module Invoke-ElevatedCommand
-Import-Module ProjectN
+Import-Module $pscxpath\pscx -force -arg @{CD_EchoNewLocation = $false}
+Import-Module $path\Invoke-ElevatedCommand
+Import-Module $path\ProjectN
 
 # Include Other script files
 $path = Join-Path $PSCommandPath ".."

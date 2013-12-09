06fb428e1b2706c1f05635ace38d8d16070833f3
Taylor Southwick <tasou@microsoft.com>
2013-12-09 09:38:46 -0800

    Adding an alias for git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,19 @@
-﻿$path="$env:userprofile\Documents\WindowsPowerShell\Modules"
+﻿# Include Other script files
+$path = Join-Path $PSCommandPath ".."
+$modulepath = Join-Path $path Modules
 $pscxpath="C:\Program Files (x86)\PowerShell Community Extensions\Pscx3"
 
-Import-Module $path\posh-git
+. $path\SetupAliases.ps1
+
+Import-Module $modulepath\posh-git
 #Import-Module PsGet
 Import-Module $pscxpath\pscx -force -arg @{CD_EchoNewLocation = $false}
-Import-Module $path\Invoke-ElevatedCommand
-Import-Module $path\ProjectN
-
-# Include Other script files
-$path = Join-Path $PSCommandPath ".."
+Import-Module $modulepath\Invoke-ElevatedCommand
+Import-Module $modulepath\ProjectN
 
 . $path\SetupVSEnvironment.ps1
 . $path\Logs.ps1
 . $path\Prompt.ps1
-. $path\SetupAliases.ps1
 . $path\Get-Checksum.ps1
 . $path\TextHelpers.ps1
 
--- SetupAliases.ps1
+++ SetupAliases.ps1
@@ -4,4 +4,5 @@ Set-Alias -Name loc -Value Copy-Location
 Set-Alias -Name npp -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 Set-Alias -Name sudo -Value Invoke-ElevatedCommand
 
+Set-Alias -Name git -Value "C:\Program Files (x86)\Git\bin\Git.exe"
 function Copy-Location  { (Get-Location).Path | Out-Clipboard }

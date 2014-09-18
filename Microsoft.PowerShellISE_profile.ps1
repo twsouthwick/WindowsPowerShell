787788f8115c0f3204ed3984137fa6d8bd02e5f9
Taylor Southwick <tasou@microsoft.com>
2014-09-18 10:40:47 -0700

    Remove extra profiles

--- Microsoft.PowerShellISE_profile.ps1
+++
@@ -1,5 +0,0 @@
-$path = Join-Path $PSCommandPath ".."
-
-. $path\Microsoft.PowerShell_profile.ps1
-
-cd ~
--- nsoftware.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-$path = Join-Path $PSCommandPath ".."
-
-. $path\Microsoft.PowerShell_profile.ps1

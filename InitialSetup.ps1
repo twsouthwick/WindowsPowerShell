2bee911e4199578db1717ec88b6073df595eb568
Taylor Southwick <tasou@microsoft.com>
2015-01-29 16:07:40 -0800

    Add script to help with InitialSetup

---
+++ InitialSetup.ps1
@@ -0,0 +1,12 @@
+Import-Module OneGet
+
+# Get PS components
+Register-PSRepository -Name tws-ps -SourceLocation https://www.myget.org/F/tws-ps/ -InstallationPolicy Trusted
+
+Find-Package -Source tws-ps | Install-Package -Scope CurrentUser
+Find-Package Pscx -Source PSGallery | Install-Package -Scope CurrentUser
+
+# Get tools
+Register-PackageSource -Name tws-tools -Location https://www.myget.org/F/tws-tools/ -Trusted  -ProviderName Chocolatey
+
+Install-Package github-cli -Source tws-tools -AllowPrereleaseVersions -MinimumVersion 2.2.0-rc1

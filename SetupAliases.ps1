05d7d062f6977009c7d6a4a00d71c0ed804bb8bb
Taylor Southwick <tasou@microsoft.com>
2015-01-30 10:55:35 -0800

    Consolidate profile into one script

--- SetupAliases.ps1
+++
@@ -1,7 +0,0 @@
-Set-Alias -Name loc  -Value Copy-Location
-Set-Alias -Name npp  -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
-Set-Alias -Name sudo -Value Invoke-ElevatedCommand
-Set-Alias -Name vim  -Value "C:\Program Files (x86)\Vim\vim74\vim.exe"
-Set-Alias -Name git  -Value "C:\Program Files (x86)\Git\bin\Git.exe"
-
-function Copy-Location  { (Get-Location).Path | Out-Clipboard }
--- profile.ps1
+++ profile.ps1
@@ -3,6 +3,9 @@ $env:PATH="$env:PATH;c:\ProgramData\chocolatey\bin\"
 
 $path = Join-Path $PSCommandPath ".."
 
+#############################################
+## Import modules                          ##
+#############################################
 if($host.Name -eq 'ConsoleHost')
 {
   Import-Module PSReadLine-vim
@@ -14,12 +17,62 @@ if($host.Name -eq 'ConsoleHost')
 
 Import-Module posh-git2
 Import-Module Pscx -force -arg @{CD_EchoNewLocation = $false}
-Import-Module Invoke-ElevatedCommand
 
-. $path\Logs.ps1
-. $path\Prompt.ps1
-. $path\SetupAliases.ps1
+#############################################
+## Setup prompt                            ##
+#############################################
+function prompt {
+	$realLASTEXITCODE = $LASTEXITCODE
+
+	# Reset color, which can be messed up by Enable-GitColors
+	$Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+	$dir = $pwd.Path.Replace("Microsoft.PowerShell.Core\FileSystem::", "");
+
+	Write-Host ("`n[$dir]") -nonewline -ForegroundColor DarkGreen
+
+	Write-VcsStatus
+
+	$global:LASTEXITCODE = $realLASTEXITCODE
+
+	return "`n$ "
+}
+
+Enable-GitColors
+
+$GitPromptSettings.EnableWindowTitle = " "
+$GitPromptSettings.WorkingForegroundColor = "Red"
+$GitPromptSettings.UntrackedForegroundColor = "Red"
+
+#############################################
+## Start transcript                        ##
+#############################################
+$variable:preference:log:path = "C:\PSLogs"
+$variable:preference:log:current_log = "$variable:preference:log:path\" + ([System.DateTime]::Now.ToString("yyyyMMdd-HHmmss")) + ".log"
+
+if(!(Test-Path $variable:preference:log:path))
+{
+	New-Item -ItemType Directory -Path $variable:preference:log:path
+}
+
+try
+{
+	Start-Transcript -Path $variable:preference:log:current_log
+	Write-Host ""
+}
+catch [Exception]
+{ }
+
+#############################################
+## Setup aliases                           ##
+#############################################
+
+Set-Alias -Name loc  -Value Copy-Location
+Set-Alias -Name npp  -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
+Set-Alias -Name vim  -Value "C:\Program Files (x86)\Vim\vim74\vim.exe"
+Set-Alias -Name git  -Value "C:\Program Files (x86)\Git\bin\Git.exe"
+
+function Copy-Location  { (Get-Location).Path | Out-Clipboard }
 
 $PROFILE = Join-Path $path "profile.ps1"
-
 $PSDefaultParameterValues["Out-Default:OutVariable"] = "0" 

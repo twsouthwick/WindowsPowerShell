2b900ca43b4a5e5a4925ae50e1daa3c977b61b93
Taylor Southwick <tasou@microsoft.com>
2015-03-06 14:28:42 -0800

    Add hub.protocol https config option

--- SetupGitConfig.ps1
+++ SetupGitConfig.ps1
@@ -3,3 +3,4 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.hist "log --decorate --stat --graph --pretty=format:'%d %Cgreen%h%Creset %s [%ar - %Cred%an%Creset]%n'"
+git config --global hub.protocol https

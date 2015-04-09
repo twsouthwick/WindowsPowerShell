8d29ba428048924decedfef5ebf5daf4e4525192
Taylor Southwick <tasou@microsoft.com>
2015-04-09 11:35:35 -0700

    Update ChocolateyInstall.ps1 path

--- Bootstrap/setup.cmd
+++ Bootstrap/setup.cmd
@@ -2,7 +2,7 @@ REM Download Chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 REM Get Chocolatey packages
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/twsouthwick/WindowsPowerShell/master/ChocolateyInstall.ps1'))"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/twsouthwick/WindowsPowerShell/master/Bootstrap/ChocolateyInstall.ps1'))"
 
 REM Setup DNX
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"

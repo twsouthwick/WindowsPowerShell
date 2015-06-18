f9e9f0bd9a902b2f7df7ca15cedaf6eac62c58bb
Taylor Southwick <twsouthwick@outlook.com>
2015-06-18 10:39:05 -0700

    Remove package search in package install

--- Bootstrap/install_psmodules.ps1
+++ Bootstrap/install_psmodules.ps1
@@ -3,8 +3,8 @@ if(-Not (Get-PSRepository -Name tws-ps -ErrorAction Ignore)){
 	Register-PSRepository -Name tws-ps -SourceLocation https://www.myget.org/F/tws-ps/ -InstallationPolicy Trusted
 }
 
-Find-Package posh-git2 -Source tws-ps | Install-Package -Scope CurrentUser -Force
-Find-Package Pscx -Source PSGallery | Install-Package -Scope CurrentUser -Force
+Install-Package posh-git2 -Source tws-ps -Scope CurrentUser -Force
+Install-Package Pscx -Source PSGallery -Scope CurrentUser -Force
 
 # Get tools - None exist right now
 #Register-PackageSource -Name tws-tools -Location https://www.myget.org/F/tws-tools/ -Trusted  -ProviderName Chocolatey

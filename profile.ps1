47197050cc8f3ef1fd6d5fa0b7040c916929fc8f
Taylor Southwick <tasou@microsoft.com>
2016-10-19 10:20:08 -0700

    Clean up coloring in profile

--- profile.ps1
+++ profile.ps1
@@ -4,22 +4,6 @@
 }
 
 #############################################
-## Set background color                    ##
-#############################################
-& {
-  $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-  $prp = New-Object System.Security.Principal.WindowsPrincipal($wid)
-  $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-  
-  if ($prp.IsInRole($adm)) {
-    $Host.UI.RawUI.Backgroundcolor = "DarkRed"
-  }
-  else{
-    $Host.UI.RawUI.Backgroundcolor = "Black"
-  }
-}
-
-#############################################
 ## Set up variables                        ##
 #############################################
 
@@ -36,18 +20,16 @@ $env:PATH="$env:PATH;c:\ProgramData\chocolatey\bin\;C:\Chocolatey\bin"
 #############################################
 if($host.Name -eq 'ConsoleHost')
 {
-  #Import-Module PSReadLine-vim
-
   Set-PSReadlineOption -EditMode Vi
   Set-PSReadlineKeyHandler -Key Tab -Function Complete
-  Set-PSReadlineOption -BellStyle Visual
+  #Set-PSReadlineOption -BellStyle Visual
 
   # Coloring gets off because we set colors in the startup script
-  Set-psreadlineoption -ResetTokenColors  
+  #Set-psreadlineoption -ResetTokenColors  
 }
 
 Import-Module poshgit2
-# Import-Module Pscx -force -arg @{CD_EchoNewLocation = $false}
+Import-Module Pscx -arg @{CD_EchoNewLocation = $false}
 
 #############################################
 ## Setup prompt                            ##
@@ -56,7 +38,8 @@ function prompt {
 	$dir = $pwd.Path.Replace("Microsoft.PowerShell.Core\FileSystem::", "");
 
 	$status = Write-GitStatus -VT100
-  $esc = [char]0x1b
+	$esc = [char]0x1b
+	
 	return "`n${esc}[0m${esc}[32;3m[$dir]${esc}[0m$status`n$ "
 }
 
